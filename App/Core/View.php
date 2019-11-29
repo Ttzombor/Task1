f490da5d0be0375cea7bdfcabae4546f3a06ba81
Ttzombor <ttzombor@gmail.com>
2019-11-29 18:26:04 +0200

    Filled App/Core/View.php

--- App/Core/View.php
+++ App/Core/View.php
@@ -1,1 +1,15 @@
 <?php
+
+class View{
+
+    //Template view you can choose;
+    public $template_view;
+
+    function generate($content_view,$template_view, $data){
+        if(!$data && is_array($data)){
+            extract(($data));
+        }
+
+        include 'App/Views/'.$template_view;
+    }
+}

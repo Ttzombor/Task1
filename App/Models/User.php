5dc803f7a44f4438f3aeb345e44c5cbf6628212a
Ttzombor <ttzombor@gmail.com>
2019-12-01 19:44:26 +0200

    Filled User.php with database structure, which will be processed, added new fetches and optimizated.

--- App/Models/User.php
+++ App/Models/User.php
@@ -1,5 +1,68 @@
 <?php
 
+
+
 class User extends Model{
+
+    public $db;
+
+    public function __construct($database)
+    {
+        $this->db = $database;
+    }
+
+    public function getAll(){
+
+        $statement = "
+            SELECT 
+                id, surname, firstname, phone, address
+            FROM
+                user;
+                ";
+
+        try{
+            $statement = $this->db->query($statement);
+            $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            return $result;
+        }catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+
+    }
+
+    public function getById($id){
+        $statement = "
+            SELECT 
+                id, firstname, lastname, firstparent_id, secondparent_id
+            FROM
+                user
+            WHERE id = ?;
+                ";
+        try{
+            $statement = $this->db->prepare($statement);
+            $statement->execute($id);
+            $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            return $result;
+        }catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    public function delete($id){
+        $statement = "
+            DELETE
+            FROM
+                user
+            WHERE id = :id;
+                ";
+        try{
+            $statement = $this->db->prepare($statement);
+            $statement->execute($id);
+            $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            return $result;
+        }catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
 
 }
